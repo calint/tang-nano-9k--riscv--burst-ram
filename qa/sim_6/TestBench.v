4e0ecb50e66ac8263fa4401a22df578319aa423c
calint <calin.tenitchi@gmail.com>
2024-05-31 17:53:02 +0200

    .

---
+++ qa/sim_6/TestBench.v
@@ -0,0 +1,197 @@
+`timescale 1ns / 1ps
+//
+`default_nettype none
+`define DBG
+
+module TestBench;
+
+  BurstRAM #(
+      .DATA_FILE("RAM.mem"),
+      .DATA_BITWIDTH(64),
+      .DEPTH_BITWIDTH(4),
+      .CYCLES_BEFORE_DATA_READY(3),
+      .BURST_COUNT(4)
+  ) burst_ram (
+      .clk(clk),
+      .rst(rst),
+      .cmd(br_cmd),
+      .cmd_en(br_cmd_en),
+      .addr(br_addr),
+      .wr_data(br_wr_data),
+      .data_mask(br_data_mask),
+      .rd_data(br_rd_data),
+      .rd_data_valid(br_rd_data_valid),
+      .busy(br_busy)
+  );
+
+  ICache #(
+      .LINE_IX_BITWIDTH(1),  // 2^1 cache lines
+      .ADDRESS_BITWIDTH(32),
+      .INSTRUCTION_BITWIDTH(32), // 4 B per instruction
+      .INSTRUCTION_IX_IN_LINE_BITWIDTH(3),  // 2^3 32 bit instructions per cache line (32B)
+      .RAM_DEPTH_BITWIDTH(4),
+      .RAM_BURST_DATA_BITWIDTH(64),
+      .RAM_BURST_DATA_COUNT(4)  // 4 * 64 bits = 32B
+      // note: size of INSTRUCTION_IX_IN_LINE_BITWIDTH and RAM_READ_BURST_COUNT must
+      //       result in same number of bytes because a cache line is loaded by the size of a burst
+  ) dut (
+      .clk(clk),
+      .clk_ram(clk),
+      .rst(rst),
+      .enable(enable),
+      .address(address),
+      .instruction(instruction),
+      .data_ready(data_ready),
+      .busy(busy),
+
+      // wiring to BurstRAM (prefix br_)
+      .br_cmd(br_cmd),
+      .br_cmd_en(br_cmd_en),
+      .br_addr(br_addr),
+      .br_wr_data(br_wr_data),
+      .br_data_mask(br_data_mask),
+      .br_rd_data(br_rd_data),
+      .br_rd_data_valid(br_rd_data_valid),
+      .br_busy(br_busy)
+  );
+
+  // wiring between BurstRAM and ICache
+  wire br_cmd;
+  wire br_cmd_en;
+  wire [3:0] br_addr;
+  wire [63:0] br_wr_data;
+  wire [7:0] br_data_mask;
+  wire [63:0] br_rd_data;
+  wire br_rd_data_valid;
+  wire br_busy;
+  // --
+
+  localparam clk_tk = 10;
+  reg clk = 0;
+  always #(clk_tk / 2) clk = ~clk;
+
+  reg rst = 1;
+
+  reg enable = 0;
+  reg [31:0] address = 0;
+  wire [31:0] instruction;
+  wire data_ready;
+  wire busy;
+
+  initial begin
+    $dumpfile("log.vcd");
+    $dumpvars(0, TestBench);
+
+    // reset
+    #clk_tk;
+    #(clk_tk / 2);
+    rst = 0;
+
+    // cache miss
+    address = 0;
+    enable = 1;
+    #clk_tk;
+    enable = 0;
+
+    while (!data_ready) #clk_tk;
+
+    if (dut.stat_cache_misses == 1) $display("test 1 passed");
+    else $display("test 1 FAILED");
+
+    if (instruction == 32'hB7C6A980) $display("test 2 passed");
+    else $display("test 2 FAILED");
+
+    // note: data may be ready before cache is finished retrieving data
+    while (busy) #clk_tk;
+
+    // cache hit
+    address = 4;
+    enable  = 1;
+    #clk_tk;
+    enable = 0;
+
+    while (!data_ready) #clk_tk;
+
+    if (dut.stat_cache_hits == 1) $display("test 3 passed");
+    else $display("test 3 FAILED");
+
+    if (instruction == 32'h3F5A2E14) $display("test 4 passed");
+    else $display("test 4 FAILED");
+
+    while (busy) #clk_tk;
+
+    // cache hit
+    address = 8;
+    enable  = 1;
+    #clk_tk;
+    enable = 0;
+
+    while (!data_ready) #clk_tk;
+
+    if (dut.stat_cache_hits == 2) $display("test 5 passed");
+    else $display("test 5 FAILED");
+
+    if (instruction == 32'hAB4C3E6F) $display("test 6 passed");
+    else $display("test 6 FAILED");
+
+    while (busy) #clk_tk;
+
+    // cache hit
+    address = 16;
+    enable  = 1;
+    #clk_tk;
+    enable = 0;
+
+    while (!data_ready) #clk_tk;
+
+    if (dut.stat_cache_hits == 3) $display("test 7 passed");
+    else $display("test 7 FAILED");
+
+    if (instruction == 32'hD5B8A9C4) $display("test 8 passed");
+    else $display("test 8 FAILED");
+
+    while (busy) #clk_tk;
+
+    // cache miss
+    address = 32;
+    enable  = 1;
+    #clk_tk;
+    enable = 0;
+
+    while (!data_ready) #clk_tk;
+
+    if (dut.stat_cache_misses == 2) $display("test 9 passed");
+    else $display("test 9 FAILED");
+
+    if (instruction == 32'h2F5E3C7A) $display("test 11 passed");
+    else $display("test 11 FAILED");
+
+    while (busy) #clk_tk;
+
+    // cache miss (eviction)
+    address = 68;
+    enable  = 1;
+    #clk_tk;
+    enable = 0;
+
+    while (!data_ready) #clk_tk;
+
+    if (dut.stat_cache_misses == 3) $display("test 12 passed");
+    else $display("test 12 FAILED");
+
+    if (instruction == 32'h0A1B2C3D) $display("test 13 passed");
+    else $display("test 13 FAILED");
+
+    while (busy) #clk_tk;
+
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+
+    // $display("cache(hits,misses): (%0d,%0d)", dut.stat_cache_hits, dut.stat_cache_misses);
+
+    $finish;
+  end
+
+endmodule
---
+++ qa/sim_7/TestBench.v
@@ -0,0 +1,240 @@
+`timescale 1ns / 1ps
+//
+`default_nettype none
+`define DBG
+
+module TestBench;
+  BurstRAM #(
+      .DATA_FILE(""),
+      .CYCLES_BEFORE_DATA_READY(4),
+      .BURST_COUNT(4),
+      .DEPTH_BITWIDTH(4)
+  ) dut (
+      .clk(clk),
+      .rst(rst),
+      .cmd(cmd),
+      .cmd_en(cmd_en),
+      .addr(addr),
+      .wr_data(wr_data),
+      .data_mask(data_mask),
+      .rd_data(rd_data),
+      .rd_data_valid(rd_data_valid),
+      .busy(busy)
+  );
+
+  localparam clk_tk = 10;
+  reg clk = 0;
+  always #(clk_tk / 2) clk = ~clk;
+
+  reg rst = 1;
+
+  reg cmd = 0;
+  reg cmd_en = 0;
+  reg [3:0] addr = 0;
+  reg [63:0] wr_data = 0;
+  reg [7:0] data_mask = 0;
+  wire [63:0] rd_data;
+  wire rd_data_valid;
+  wire busy;
+
+  initial begin
+    $dumpfile("log.vcd");
+    $dumpvars(0, TestBench);
+
+    // reset
+    #clk_tk;
+    #(clk_tk / 2);
+    rst = 0;
+
+    // write to 0x0000
+    cmd_en = 1;
+    cmd = 1; // write
+    addr = 0;
+    wr_data = 64'h3F5A2E14B7C6A980;
+    #clk_tk;
+    cmd_en = 0; // note: can be disabled
+    cmd = 0; // note: can be disabled
+    wr_data = 64'h9D8E2F17AB4C3E6F;
+    #clk_tk;
+    wr_data = 64'hA1C3F7E2D5B8A9C4;
+    #clk_tk;
+    wr_data = 64'h7D4E9F2C1B6A3D8F;
+    #clk_tk;
+
+    // test busy
+    #clk_tk;
+
+    if (!busy) $display("test 1 passed");
+    else $display("test 1 FAILED");
+
+    // test writing knowing when busy if false
+
+    // write to 0x0020
+    cmd_en = 1;
+    cmd = 1; // write
+    addr = 32 / 8; // 8 bytes words
+    wr_data = 64'h6C4B9A8D2F5E3C7A;
+    #clk_tk;
+    cmd_en = 0; // note: can be disabled
+    cmd = 0; // note: can be disabled
+    wr_data = 64'hE1A7D0B5C8F3E6A9;
+    #clk_tk;
+    wr_data = 64'hF8E9D2C3B4A5F6E7;
+    #clk_tk;
+    wr_data = 64'hD4E7F2C5B8A3D6E9;
+    #clk_tk;
+
+    // write to 0x0000
+    cmd_en = 1;
+    cmd = 1; // write
+    addr = 0;
+    wr_data = 64'h3F5A2E14B7C6A980;
+    #clk_tk;
+    cmd_en = 0; // note: can be disabled
+    cmd = 0; // note: can be disabled
+    wr_data = 64'h9D8E2F17AB4C3E6F;
+    #clk_tk;
+    wr_data = 64'hA1C3F7E2D5B8A9C4;
+    #clk_tk;
+    wr_data = 64'h7D4E9F2C1B6A3D8F;
+    #clk_tk;
+
+    // read 0x0000
+    cmd_en = 1;
+    cmd = 0;
+    addr = 0;
+    #clk_tk;
+    cmd_en = 0; // note: can be disable
+    cmd = 0; // note: can be disabled
+    #clk_tk;
+
+    // delay before burst (DELAY_BEFORE_RD_DATA_AVAILABLE)
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+
+    // check data ready
+    if (rd_data_valid) $display("test 1 passed");
+    else $display("test 1 FAILED");
+
+    // first data
+    if (rd_data == 64'h3F5A2E14B7C6A980) $display("test 2 passed");
+    else $display("test 2 FAILED");
+
+    #clk_tk;
+
+    // second data
+    if (rd_data == 64'h9D8E2F17AB4C3E6F) $display("test 3 passed");
+    else $display("test 3 FAILED");
+
+    #clk_tk;
+
+    // third data
+    if (rd_data == 64'hA1C3F7E2D5B8A9C4) $display("test 4 passed");
+    else $display("test 4 FAILED");
+
+    #clk_tk;
+
+    // fourth data
+    if (rd_data == 64'h7D4E9F2C1B6A3D8F) $display("test 5 passed");
+    else $display("test 5 FAILED");
+
+    #clk_tk;
+
+    // check data ready
+    if (!rd_data_valid) $display("test 6 passed");
+    else $display("test 6 FAILED");
+
+    // read 0x0020
+    cmd_en = 1;
+    cmd = 0;
+    addr = 32 / 8;  // 8 bytes words
+    #clk_tk;
+    cmd_en = 0; // note: can be disable
+    cmd = 0; // note: can be disabled
+    #clk_tk;
+
+    // delay before burst (DELAY_BEFORE_RD_DATA_AVAILABLE)
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+
+    // check data ready
+    if (rd_data_valid) $display("test 7 passed");
+    else $display("test 7 FAILED");
+
+    // first data
+    if (rd_data == 64'h6C4B9A8D2F5E3C7A) $display("test 8 passed");
+    else $display("test 8 FAILED");
+
+    #clk_tk;
+
+    // second data
+    if (rd_data == 64'hE1A7D0B5C8F3E6A9) $display("test 9 passed");
+    else $display("test 9 FAILED");
+
+    #clk_tk;
+
+    // third data
+    if (rd_data == 64'hF8E9D2C3B4A5F6E7) $display("test 10 passed");
+    else $display("test 10 FAILED");
+
+    #clk_tk;
+
+    // fourth data
+    if (rd_data == 64'hD4E7F2C5B8A3D6E9) $display("test 11 passed");
+    else $display("test 11 FAILED");
+
+    // read 0x0000
+    cmd_en = 1;
+    cmd = 0;
+    addr = 0;
+    #clk_tk;
+    cmd_en = 0; // note: can be disable
+    cmd = 0; // note: can be disabled
+    #clk_tk;
+
+    // delay before burst (DELAY_BEFORE_RD_DATA_AVAILABLE)
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+
+    // check data ready
+    if (rd_data_valid) $display("test 12 passed");
+    else $display("test 12 FAILED");
+
+    // first data
+    if (rd_data == 64'h3F5A2E14B7C6A980) $display("test 13 passed");
+    else $display("test 13 FAILED");
+
+    #clk_tk;
+
+    // second data
+    if (rd_data == 64'h9D8E2F17AB4C3E6F) $display("test 14 passed");
+    else $display("test 14 FAILED");
+
+    #clk_tk;
+
+    // third data
+    if (rd_data == 64'hA1C3F7E2D5B8A9C4) $display("test 15 passed");
+    else $display("test 15 FAILED");
+
+    #clk_tk;
+
+    // fourth data
+    if (rd_data == 64'h7D4E9F2C1B6A3D8F) $display("test 16 passed");
+    else $display("test 16 FAILED");
+
+    // some waits at the ends
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+    #clk_tk;
+
+    $finish;
+  end
+
+endmodule
