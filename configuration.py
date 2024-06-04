a8520b07158e55d3cc3c552820b07eb369cf6542
calint <calin.tenitchi@gmail.com>
2024-06-04 20:01:35 +0200

    .

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 RAM_FILE = "os/os.mem" # path to initial RAM content
-RAM_ADDR_WIDTH = 11    # 2 ^ 13 * 4 * 2 (65636) bytes of 64 bit PSRAM
+RAM_ADDR_WIDTH = 10    # 2 ^ 10 * 4 * 2 (8192) bytes of 64 bit PSRAM
 UART_BAUD_RATE = 9600  # 9600 baud, 8 bits, 1 stop bit, no parity
 
--- src/Configuration.v
+++ src/Configuration.v
@@ -1,4 +1,4 @@
 // generated - do not edit
 `define RAM_FILE "../os/os.mem"
-`define RAM_ADDR_WIDTH 11
+`define RAM_ADDR_WIDTH 10
 `define UART_BAUD_RATE 9600
